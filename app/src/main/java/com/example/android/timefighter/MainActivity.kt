6b6ab6ad527852e55710e47e037f7f4a3061a9fd
devin.c.north <devin.c.north@accenture.com>
2020-01-15 15:16:56 -0600

    Completed layout and game. Probably not how they implemented it, but wanted to see if I could do it on my own before I learned how the tutorial does it.

--- app/src/main/java/com/example/android/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/android/timefighter/MainActivity.kt
@@ -4,23 +4,68 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.Timer
+import java.util.TimerTask
+
+class GameSecond(private val task: () -> Unit) :
+    TimerTask() {
+
+    override fun run() {
+        task()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    private var clicks = 0
+    private var taps = 0
+    private var count = 0
+    private var best = 0
+    private var gameOn = false
+    private lateinit var timer: Timer
+    private lateinit var timerText: TextView
+    private lateinit var tapsText: TextView
+    private lateinit var bestText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timerText = findViewById(R.id.time_left)
+        tapsText = findViewById(R.id.taps)
+        bestText = findViewById(R.id.best)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             handleClick()
         }
     }
 
+    private fun setReadoutText(textView: TextView, stringId: Int, readout: String) {
+        textView.text = getString(stringId, readout)
+    }
+
     private fun handleClick() {
-        val textOutput = findViewById<TextView>(R.id.clicks)
-        clicks += 1
-        textOutput.text = clicks.toString()
+        if (gameOn) {
+            taps++
+            setReadoutText(tapsText, R.string.taps, taps.toString())
+        } else {
+            gameOn = true
+            count = 10
+            setReadoutText(timerText, R.string.time_left, count.toString())
+            timer = Timer()
+            val task = GameSecond {
+                count--
+                setReadoutText(timerText, R.string.time_left, count.toString())
+                if (count == 0) {
+                    gameOn = false
+                    best = if (taps > best) taps else best
+                    setReadoutText(bestText, R.string.best, best.toString())
+                    taps = 0
+                    setReadoutText(tapsText, R.string.taps, taps.toString())
+                    timer.cancel()
+                }
+            }
+            setReadoutText(timerText, R.string.time_left, 10.toString())
+            setReadoutText(tapsText, R.string.taps, 0.toString())
+            timer.scheduleAtFixedRate(task, 1000, 1000)
+        }
     }
 }
